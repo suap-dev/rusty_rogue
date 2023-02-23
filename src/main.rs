c9ed4bfbf62d2b7597d97115791625ecda27445c
suap-dev <suap.dev@gmail.com>
2023-02-23 23:31:43 +0100

    Hello World!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 #![warn(clippy::all, clippy::pedantic)]
 
-fn main() {
-    println!("Hello, world!");
+use bracket_lib::prelude::*;
+
+const WINDOW_HEIGHT: i16 = 50;
+const WINDOW_WIDTH: i16 = 80;
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Rusty Rogue")
+        .build()?;
+
+    main_loop(context, RustyRogue {})
+}
+
+struct RustyRogue {}
+impl GameState for RustyRogue {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.print_centered(WINDOW_HEIGHT / 2, "Hello World!");
+    }
 }
