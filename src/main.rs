df95bc5fcef01f4399b327c598f33e2715e0020f
suap-dev <suap.dev@gmail.com>
2023-02-26 21:04:42 +0100

    looks bit better

--- src/main.rs
+++ src/main.rs
@@ -19,11 +19,26 @@ use prelude::*;
 
 const SCREEN_HEIGHT: i32 = 50;
 const SCREEN_WIDTH: i32 = 80;
+
+const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
+const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
+
 const NUM_ROOMS: i32 = 20;
 
 fn main() -> BError {
-    let context = BTermBuilder::simple80x50()
+    // let context = BTermBuilder::simple80x50()
+    let context = BTermBuilder::new()
         .with_title("Rusty Rogue")
+        .with_fps_cap(30.0)
+        // .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+        .with_dimensions(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .with_tile_dimensions(16, 16)
+        .with_resource_path("resources/")
+        .with_font("dungeonfont.png", 32, 32)
+        // .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        // .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_simple_console(SCREEN_WIDTH, SCREEN_HEIGHT, "dungeonfont.png")
+        // .with_simple_console_no_bg(SCREEN_WIDTH, SCREEN_HEIGHT, "dungeonfont.png")
         .build()?;
 
     main_loop(context, RustyRogue::new())
