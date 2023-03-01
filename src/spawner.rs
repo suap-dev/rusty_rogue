9e0305f904f2a1f361c635b1936e5a4a8f6db4fc
suap-dev <suap.dev@gmail.com>
2023-03-01 17:06:45 +0100

    add enemies to spawner

--- src/spawner.rs
+++ src/spawner.rs
@@ -1,5 +1,17 @@
 use crate::prelude::*;
 
+const ENEMY_GLYPHS: [FontCharType; 4] = [glyph::ETTIN, glyph::OGRE, glyph::ORC, glyph::GOBLIN];
+pub fn spawn_monster(world: &mut World, rng: &mut RandomNumberGenerator, pos: Point) {
+    world.push((
+        Enemy,
+        pos,
+        Render {
+            color: DEFAULT_COLOR,
+            glyph: ENEMY_GLYPHS[rng.range(0, 4)],
+        },
+    ));
+}
+
 pub fn spawn_player(world: &mut World, pos: Point) {
     world.push((
         Player,
